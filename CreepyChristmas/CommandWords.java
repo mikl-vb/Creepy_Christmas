44d13fdfe7da4dee5d14290f895140de6b4d9dda
msvb93 <msvb93@gmail.com>
2014-11-30 23:12:41 -0500

    adding changes

--- CreepyChristmas/CommandWords.java
+++ CreepyChristmas/CommandWords.java
@@ -13,7 +13,7 @@ public class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String[] validCommands = {
-        "go", "quit", "help", "grab", "forward ", "kick", "slash"
+        "go", "quit", "help", "grab", "forward ", "kick", "slash", "run", "stay", "still"
     };
 
     /**
